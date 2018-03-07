bfba8495a1d9087f3d5dd380eccda71243c5a75e
shenmingming1 <945884326@qq.com>
2018-03-07 11:28:54 +0800

    上传framework

---
+++ testFrameWork.framework/Versions/1/Headers/TestadditionOneNum.h
@@ -0,0 +1,13 @@
+//
+//  TestadditionOneNum.h
+//  testFrameWork
+//
+//  Created by 58 on 2018/3/7.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestadditionOneNum : NSObject
++ (NSInteger)additionOneNum:(NSInteger)oneNum twoNum:(NSInteger)twoNum;
+@end
---
+++ testFrameWork.framework/Versions/1/Headers/testFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  testFrameWork.h
+//  testFrameWork
+//
+//  Created by 58 on 2018/3/7.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TestadditionOneNum.h"
+//! Project version number for testFrameWork.
+FOUNDATION_EXPORT double testFrameWorkVersionNumber;
+
+//! Project version string for testFrameWork.
+FOUNDATION_EXPORT const unsigned char testFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFrameWork/PublicHeader.h>
+
+
